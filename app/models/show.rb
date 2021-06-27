1b97c4d392271b86908090566342d85ee5763b66
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-27 17:27:52 +0000

    write actors_list function

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
     has_many :actors, through: :characters
 
     def actors_list
-        
+        self.characters.map{|character| character.actor.full_name}
     end
 end
